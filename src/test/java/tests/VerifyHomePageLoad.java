0983090680b6c2a97d2124c8c699db9a13bcfa18
Arild Andreassen <arildsa@hotmail.com>
2017-10-22 18:17:14 -0600

    Added assertions in the homepage test to verify links where present

---
+++ src/test/java/tests/VerifyHomePageLoad.java
@@ -0,0 +1,29 @@
+package tests;
+
+import base.BaseTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class VerifyHomePageLoad extends BaseTest{
+
+    @Test
+    public void verifyHomePageLoadTest()
+    {
+        Assert.assertTrue(homePage.isImgCentralLogoPresent());
+        Assert.assertTrue(homePage.isSearchBarPresent());
+        Assert.assertTrue(homePage.isSearchButtonPresent());
+        Assert.assertTrue(homePage.isLanguageListPresent());
+        Assert.assertTrue(homePage.isThisLinkInTheFooter("//commons.wikimedia.org/"));
+        Assert.assertTrue(homePage.isThisLinkInTheFooter("//www.wikivoyage.org/"));
+        Assert.assertTrue(homePage.isThisLinkInTheFooter("//www.wiktionary.org/"));
+        Assert.assertTrue(homePage.isThisLinkInTheFooter("//www.wikibooks.org/"));
+        Assert.assertTrue(homePage.isThisLinkInTheFooter("//www.wikinews.org/"));
+        Assert.assertTrue(homePage.isThisLinkInTheFooter("//www.wikidata.org/"));
+        Assert.assertTrue(homePage.isThisLinkInTheFooter("//www.wikiversity.org/"));
+        Assert.assertTrue(homePage.isThisLinkInTheFooter("//www.wikiquote.org/"));
+        Assert.assertTrue(homePage.isThisLinkInTheFooter("//www.mediawiki.org/"));
+        Assert.assertTrue(homePage.isThisLinkInTheFooter("//www.wikisource.org/"));
+        Assert.assertTrue(homePage.isThisLinkInTheFooter("//species.wikimedia.org/"));
+        Assert.assertTrue(homePage.isThisLinkInTheFooter("//meta.wikimedia.org/"));
+    }
+}
