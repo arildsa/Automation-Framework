2b81a92d9d6208cd695182aaed4e6f4febf256ec
Arild Andreassen <arildsa@hotmail.com>
2017-10-22 20:33:18 -0600

    Initial Rest Api commit

---
+++ src/test/java/REST/ResponseHeaderData.java
@@ -0,0 +1,26 @@
+package REST;
+
+import base.RESTBase;
+import io.restassured.http.ContentType;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ResponseHeaderData extends RESTBase {
+
+    @Test
+    public void test_ResponseHeaderData_ShouldBeCorrect() {
+
+        given().
+                when().
+                get(getRestUrl() + "/api/f1/2017/circuits.json").
+                then().
+                assertThat().
+                statusCode(200).
+                and().
+                contentType(ContentType.JSON).
+                and().
+                header("Content-Length",equalTo("4551"));
+    }
+}
