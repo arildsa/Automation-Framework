b0113c0924885eca51f1cb25ee01187d525a6a57
Arild Andreassen <arildsa@hotmail.com>
2017-10-28 15:54:13 -0600

    Made changes to the CircleCi config

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -14,6 +14,8 @@ import rules.RetryOnFailure;
 import rules.ScreenShotOnFailure;
 import util.CustomCommands;
 
+import java.io.File;
+
 public class BaseTest extends CustomCommands{
 
     private static RemoteWebDriver driver;
@@ -24,13 +26,21 @@ public class BaseTest extends CustomCommands{
     @BeforeClass
     public static void beforeClass()
     {
+        File file = null;
         if (System.getProperty("os.name").contains("Mac")){
             System.setProperty("webdriver.chrome.driver", "lib/chromedriverMac.exe");
+            file = new File("lib/chromedriverMac.exe");
         } else if (System.getProperty("os.name").contains("Linux")) {
             System.setProperty("webdriver.chrome.driver", "lib/chromedriverLinux.exe");
+            file = new File("lib/chromedriverLinux.exe");
         } else if(System.getProperty("os.name").contains("Windows")) {
             System.setProperty("webdriver.chrome.driver","lib/chromedriver.exe");
+            file = new File("lib/chromedriver.exe");
         }
+
+        file.setExecutable(true);
+        file.setReadable(true);
+        file.setWritable(true);
 
         DesiredCapabilities capabilities = DesiredCapabilities.chrome();
         capabilities.setCapability(CapabilityType.BROWSER_NAME,"Chrome");
